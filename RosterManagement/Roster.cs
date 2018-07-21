dad7ddc2ac95fa74648dda9ecc732f8b99e5be57
Nikita karanpuria <nikita.karanpuria8@gmail.com>
2018-07-21 10:15:48 +0530

    done

--- RosterManagement/Roster.cs
+++ RosterManagement/Roster.cs
@@ -1,45 +1,45 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-
 namespace RosterManagement
 {
     public class CodeSchool
     {
         Dictionary<int, List<string>> _roster;
-
         public CodeSchool()
         {
-            _roster = new Dictionary<int, List<String>>();
+          this._roster = new Dictionary<int, List<String>>();
         }
-        
-        /// <summary>
-        /// Should be able to Add Student to a Particular Wave
-        /// </summary>
-        /// <param name="cadet">Refers to the name of the Cadet</param>
-        /// <param name="wave">Refers to the Wave number</param>
         public void Add(string cadet, int wave)
         {
+             if (_roster.ContainsKey(wave))
+                 _roster[wave].Add(cadet);
+             else
+                 _roster.Add(wave, new List<string> { cadet });
+             _roster[wave].Sort();
         }
-
-        /// <summary>
-        /// Returns all the Cadets in a given wave
-        /// </summary>
-        /// <param name="wave">Refers to the wave number from where cadet list is to be fetched</param>
-        /// <returns>List of Cadet's Name</returns>
         public List<string> Grade(int wave)
         {
             var list = new List<string>();
-            return list;
+           
+            if (_roster.ContainsKey(wave))
+            {
+                return _roster[wave];
+            }
+            else
+            {
+                return list;
+            }
         }
-
-        /// <summary>
-        /// Return all the cadets in the CodeSchool
-        /// </summary>
-        /// <returns>List of Cadet's Name</returns>
         public List<string> Roster()
         {
             var cadets = new List<string>();
+              var sorted_index = _roster.Keys.ToList();
+            sorted_index.Sort();
+            foreach (int item in sorted_index)
+            {
+                cadets.AddRange(_roster[item]);
+            }
             return cadets;
         }
     }
